56b21408ea01d7ed8f884b5cf48240eed9efc0a3
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2025-12-03 22:53:12 -0300

    refact: main

--- codigos/src/arvore_geradora_minima/App.java
+++ codigos/src/arvore_geradora_minima/App.java
@@ -7,13 +7,26 @@ import java.util.PriorityQueue;
 
 public class App {
     public static void main(String[] args) {
-        List<List<int[]>> grafo = listaAdj();
+        List<List<int[]>> grafo = listaAdj1();
+        List<List<int[]>> grafo2 = listaAdj2();
 
         List<Aresta> agmKrl = kruskalAGM(grafo);
         List<Aresta> agmPrm = primAGM(grafo);
 
+        List<Aresta> agmKrl2 = kruskalAGM(grafo2);
+        List<Aresta> agmPrm2 = primAGM(grafo2);
+
+        System.out.println("\n\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+        System.out.println("Grafo 1");
+
         exibirAGM(agmKrl, "Kruskal");
         exibirAGM(agmPrm, "Prim");
+
+        System.out.println("\n\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+        System.out.println("Grafo 2");
+
+        exibirAGM(agmKrl2, "Kruskal");
+        exibirAGM(agmPrm2, "Prim");
     }
 
     public static void exibirAGM(List<Aresta> agm, String agmTipo){
@@ -25,12 +38,12 @@ public class App {
 
         for (Aresta e : agm) {
             System.out.println(e.u + " - " + e.v + " (" + e.peso + ")");
-            peso =+ e.peso;
+            peso += e.peso;
         }
         System.out.println("Peso total: " + peso);
     }
 
-    public static List<List<int[]>> listaAdj(){
+    public static List<List<int[]>> listaAdj1(){
         List<List<int[]>> grafo = new ArrayList<>();
         
         int n = 4;
@@ -51,6 +64,82 @@ public class App {
         return grafo;
     } 
 
+
+    public static List<List<int[]>> listaAdj2(){
+
+        
+        int n = 12;
+
+        List<List<int[]>> grafo = new ArrayList<>();
+        for (int i = 0; i < n; i++) grafo.add(new ArrayList<>());
+
+        // --- Região A (0,1,2,3) ---
+        grafo.get(0).add(new int[]{1, 4});
+        grafo.get(1).add(new int[]{0, 4});
+
+        grafo.get(0).add(new int[]{2, 6});
+        grafo.get(2).add(new int[]{0, 6});
+
+        grafo.get(1).add(new int[]{2, 3});
+        grafo.get(2).add(new int[]{1, 3});
+
+        grafo.get(1).add(new int[]{3, 5});
+        grafo.get(3).add(new int[]{1, 5});
+
+        grafo.get(2).add(new int[]{3, 7});
+        grafo.get(3).add(new int[]{2, 7});
+
+        // --- Região B (4,5,6,7) ---
+        grafo.get(4).add(new int[]{5, 2});
+        grafo.get(5).add(new int[]{4, 2});
+
+        grafo.get(4).add(new int[]{6, 8});
+        grafo.get(6).add(new int[]{4, 8});
+
+        grafo.get(5).add(new int[]{6, 4});
+        grafo.get(6).add(new int[]{5, 4});
+
+        grafo.get(5).add(new int[]{7, 6});
+        grafo.get(7).add(new int[]{5, 6});
+
+        grafo.get(6).add(new int[]{7, 3});
+        grafo.get(7).add(new int[]{6, 3});
+
+        // --- Região C (8,9,10,11) ---
+        grafo.get(8).add(new int[]{9, 1});
+        grafo.get(9).add(new int[]{8, 1});
+
+        grafo.get(8).add(new int[]{10, 5});
+        grafo.get(10).add(new int[]{8, 5});
+
+        grafo.get(9).add(new int[]{10, 2});
+        grafo.get(10).add(new int[]{9, 2});
+
+        grafo.get(9).add(new int[]{11, 7});
+        grafo.get(11).add(new int[]{9, 7});
+
+        grafo.get(10).add(new int[]{11, 4});
+        grafo.get(11).add(new int[]{10, 4});
+
+        // --- Ligações entre as regiões ---
+        grafo.get(3).add(new int[]{4, 9});
+        grafo.get(4).add(new int[]{3, 9});
+
+        grafo.get(2).add(new int[]{5, 10});
+        grafo.get(5).add(new int[]{2, 10});
+
+        grafo.get(7).add(new int[]{8, 3});
+        grafo.get(8).add(new int[]{7, 3});
+
+        grafo.get(6).add(new int[]{9, 6});
+        grafo.get(9).add(new int[]{6, 6});
+
+        grafo.get(3).add(new int[]{11, 11});
+        grafo.get(11).add(new int[]{3, 11});
+
+        return grafo;
+    } 
+
     public static List<Aresta> primAGM(List<List<int[]>> grafo) {
 
         int n = grafo.size();
@@ -58,34 +147,27 @@ public class App {
 
         List<Aresta> agm = new ArrayList<>();
 
-        // Min-heap ordenada pelo peso
         PriorityQueue<Aresta> heap = new PriorityQueue<>(
             (a, b) -> a.peso - b.peso
         );
 
-        // 1. Começamos do vértice 0
         visitado[0] = true;
 
-        // Adiciona todas as arestas saindo do 0
         for (int[] par : grafo.get(0)) {
             heap.add(new Aresta(0, par[0], par[1]));
         }
 
-        // 2. Prim
         while (!heap.isEmpty() && agm.size() < n - 1) {
 
             Aresta menor = heap.poll();
 
             int v = menor.v;
 
-            // Se o destino já foi visitado, ignoramos
             if (visitado[v]) continue;
 
-            // Aceita a aresta
             agm.add(menor);
             visitado[v] = true;
 
-            // Adiciona as arestas do novo vértice
             for (int[] par : grafo.get(v)) {
                 if (!visitado[par[0]]) {
                     heap.add(new Aresta(v, par[0], par[1]));
@@ -101,29 +183,23 @@ public class App {
         int n = grafo.size();
         List<Aresta> arestas = new ArrayList<>();
 
-        // 1. Extrair arestas da lista de adjacência
         for (int u = 0; u < n; u++) {
             for (int[] par : grafo.get(u)) {
                 int v = par[0];
                 int peso = par[1];
 
-                // Evitar duplicar (u,v) e (v,u)
                 if (u < v) {
                     arestas.add(new Aresta(u, v, peso));
                 }
             }
         }
 
-        // 2. Ordenar pelo peso
         Collections.sort(arestas);
 
-        // 3. Union-Find
         UnionFind uf = new UnionFind(n);
 
-        // 4. AGM (resultado)
         List<Aresta> agm = new ArrayList<>();
 
-        // 5. Kruskal
         for (Aresta e : arestas) {
             if (uf.find(e.u) != uf.find(e.v)) {
                 uf.union(e.u, e.v);
